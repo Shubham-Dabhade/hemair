a0fab48e766d4f5e7aad98871f8a3c6419c3d969
Shubham Dabhade <dabahdeshubham83@gmail.com>
2023-12-04 09:50:19 +0530

    made 4 partciles landing page

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,24 @@
+import React, { useCallback, useEffect, useRef } from "react";
+import "./home.css";
+// import Particles from "react-particles";
+import { loadSlim } from "tsparticles-slim";
+import Particle1 from "../../Components/Particle1/Particle1";
+import Particle2 from "../../Components/Particle2/Particle2";
+import Particle3 from "../../Components/Particle3/Particle3";
+import Particle4 from "../../Components/Particle4/Particle4";
+
+
+
+const Home = () => {
+
+  
+
+  return (
+    // <Particle1/>
+    // <Particle2/>
+    // <Particle3/>
+    <Particle4/>
+  );
+};
+
+export default Home;
